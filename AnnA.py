ed37acd48996537e01c20f0020f7019f81426761
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-04-09 20:45:24 +0200

    add spacing to the documentation

--- AnnA.py
+++ AnnA.py
@@ -183,11 +183,13 @@ class AnnA:
     ---------
 
     -h, --help            show this help message and exit
+
     --deckname DECKNAME   the deck containing the cards you want to review. If
                           you don't supply this value or make a mistake, AnnA
                           will ask you to type in the deckname, with
                           autocompletion enabled (use `<TAB>`). Default is
                           `None`.
+
     --reference_order REF_ORDER
                           either "relative_overdueness" or "lowest_interval" or
                           "order_added" or "LIRO_mix". It is the reference used
@@ -208,6 +210,7 @@ class AnnA:
                           its interval, they are urgent. AnnA will add a tag to
                           them and increase their likelyhood of being part of
                           the Optideck.
+
     --task TASK           can be 'filter_review_cards',
                                  'bury_excess_learning_cards',
                                  'bury_excess_review_cards',
@@ -222,20 +225,24 @@ class AnnA:
                                 or create a 2D plot using BERTopic
                                 or create the 2D plot using BERTopic then add the topics as tags to the cards
                           Default is `filter_review_cards`.
+
     --target_deck_size TARGET_SIZE
                           indicates the size of the filtered deck to create. Can
                           be the number of due cards like "100", a proportion of
                           due cards like '80%', the word "all" or "deck_config"
                           to use the deck's settings for max review. Default is
                           `deck_config`.
+
     --max_deck_size MAX_DECK_SIZE
                           Maximum number of cards to put in the filtered deck or
                           to leave unburied. Default is `None`.
+
     --stopwords_lang STOPLANG [STOPLANG ...]
                           a comma separated list of languages used to construct
                           a list of stop words (i.e. words that will be ignored,
                           like "I" or "be" in English). Default is `english
                           french`.
+
     --rated_last_X_days RATED_LAST_X_DAYS
                           indicates the number of passed days to take into
                           account when fetching past anki sessions. If you rated
@@ -251,12 +258,14 @@ class AnnA:
                           rated in the last X days, no matter if they are
                           reviews or learnings. you can change this using
                           "highjack_rated_query" argument.
+
     --score_adjustment_factor SCORE_ADJUSTMENT_FACTOR [SCORE_ADJUSTMENT_FACTOR ...]
                           a comma separated list of numbers used to adjust the
                           value of the reference order compared to how similar
                           the cards are. Default is `1,5`. For example: '1, 1.3'
                           means that the algorithm will spread the similar cards
                           farther apart.
+
     --field_mapping FIELD_MAPPING_PATH
                           path of file that indicates which field to keep from
                           which note type and in which order. Default value is
@@ -266,6 +275,7 @@ class AnnA:
                           `["take_all_fields]`, AnnA will grab all fields of the
                           notetype in the same order as they appear in Anki's
                           interface.
+
     --acronym_file ACRONYM_FILE_PATH
                           a python file containing dictionaries that themselves
                           contain acronyms to extend in the text of cards. For
@@ -274,16 +284,19 @@ class AnnA:
                           Default is `"utils/acronym_example.py"`. The matching
                           is case sensitive only if the key contains uppercase
                           characters. The ".py" file extension is not mandatory.
+
     --acronym_list ACRONYM_LIST [ACRONYM_LIST ...]
                           a comma separated list of name of dictionaries to
                           extract file supplied in `acronym_file` var. Used to
                           extend text, for instance
                           `AI_machine_learning,medical_terms`. Default to None.
+
     --minimum_due MINIMUM_DUE_CARDS
                           stops AnnA if the number of due cards is inferior to
                           this value. Default is `5`. If it's an int less or
                           equal to 2 it will be set to 3 as it makes no sense
                           otherwise.
+
     --highjack_due_query HIGHJACK_DUE_QUERY
                           bypasses the browser query used to find the list of
                           due cards. You can set it for example to
@@ -291,6 +304,7 @@ class AnnA:
                           `None`. **Keep in mind that, when highjacking queries,
                           you have to specify the deck otherwise AnnA will
                           compare your whole collection.**
+
     --highjack_rated_query HIGHJACK_RATED_QUERY
                           same idea as above, bypasses the query used to fetch
                           rated cards in anki. Related to `highjack_due_query`.
@@ -305,6 +319,7 @@ class AnnA:
                           is gradually decreased as the number of days since the
                           review grows. In short it's doing temporal
                           discounting. Default is `None`.
+
     --low_power_mode      enable to reduce the computation needed for AnnA,
                           making it usable for less powerful computers. This can
                           greatly reduce accuracy. Also removes non necessary
@@ -312,20 +327,25 @@ class AnnA:
                           Specifically it uses binary mode for TFIDF and has
                           no effect if another vectorizer is used.
                           Default to `False`.
+
     --log_level LOG_LEVEL
                           can be any number between 0 and 2. Default is `0` to
                           only print errors. 1 means print also useful
                           information and >=2 means print everything. Messages
                           are color coded so it might be better to leave it at 3
                           and just focus on colors.
+
     --replace_greek       if True, all greek letters will be replaced with a
                           spelled version. For example `σ` becomes `sigma`.
                           Default is `True`.
+
     --keep_OCR            if True, the OCR text extracted using the great
                           AnkiOCR addon (https://github.com/cfculhane/AnkiOCR/)
                           will be included in the card. Default is `True`.
+
     --append_tags         Whether to append the tags to the cards content or to
                           add no tags. Default to `False`.
+
     --tags_to_ignore [TAGS_TO_IGNORE ...]
                           a list of regexp of tags to ignore when appending tags
                           to cards. This is not a list of tags whose card should
@@ -333,6 +353,7 @@ class AnnA:
                           to disable it.
                           If a string is supplied, it will be parsed as a list
                           as a comma separated value.
+
     --add_KNN_to_field    Whether to add a query to find the K nearestneighbor
                           of a given card to a new field called
                           'Nearest_neighbors' (only if already present in the
@@ -343,22 +364,28 @@ class AnnA:
                           the KNN only for the cards of the deck in question and
                           that are currently due. If you want to run this on the
                           complete deck you should use the 'task' argument.
+
     --filtered_deck_name_template FILTER_DECK_NAME_TEMPLATE
                           name template of the filtered deck to create. Only
                           available if task is set to "filter_review_cards".
                           Default is `None`.
+
     --filtered_deck_at_top_level
                           If True, the new filtered deck will be a top level
                           deck, if False: the filtered deck will be next to the
                           original deck. Default to True.
+
     --filtered_deck_by_batch
                           To enable creating batch of filtered decks. Default is
                           `False`.
+
     --filtered_deck_batch_size FILTERED_DECK_BATCH_SIZE
                           If creating batch of filtered deck, this is the number
                           of cards in each. Default is `25`.
+
     --show_banner         used to display a nice banner when instantiating the
                           collection. Default is `True`.
+
     --repick_task REPICK_TASK
                           Define what happens to cards deemed urgent in
                           'relative_overdueness' ref mode. If contains 'boost',
@@ -367,6 +394,7 @@ class AnnA:
                           indicating which card is urgent will be added at the
                           end of the run. Disable by setting it to None. Default
                           is `boost`.
+
     --vectorizer VECTORIZER
                           Either TFIDF or 'embeddings' to use
                           sentencetransformers. The latter will deduplicate the
@@ -375,12 +403,15 @@ class AnnA:
                           recomputing the embedding for previously seen notes,
                           ignore stopwords and any TFIDF arguments used.
                           Default to 'embeddings'.
+
     --sentencetransformers_device
                           either "cpu" or "gpu"/"cuda". None to guess. Default to None.
+
     --embed_model EMBED_MODEL
                           For multilingual use 'paraphrase-multilingual-mpnet-base-v2'
                            but for anything else use 'all-mpnet-
                           base-v2'
+
     --ndim_reduc NDIM_REDUC
                           the number of dimension to keep using TruncatedSVD (if
                           TFIDF) or PCA (if embeddings). If 'auto' will
@@ -392,11 +423,13 @@ class AnnA:
                           (More information at https://scikit-learn.org/stable/m
                           odules/generated/sklearn.decomposition.TruncatedSVD.ht
                           ml).
+
     --TFIDF_tokenize      default to `True`. Enable sub word tokenization, for
                           example turn `hypernatremia` to `hyp + er + natr +
                           emia`. You cannot enable both `TFIDF_tokenize` and
                           `TFIDF_stem` but should absolutely enable at least
                           one.
+
     --TFIDF_tknizer_model TFIDF_tknizer_model
                           default to `GPT`. Model to use for tokenizing the text
                           before running TFIDF. Possible values are 'bert' and
@@ -404,20 +437,25 @@ class AnnA:
                           multilingual-cased` and `gpt_neox_20B` They should
                           work on just about any languages. Use 'Both' to
                           concatenate both tokenizers. (experimental)
+
     --TFIDF_stem          default to `False`. Whether to enable stemming of
                           words. Currently the PorterStemmer is used, and was
                           made for English but can still be useful for some
                           other languages. Keep in mind that this is the longest
                           step when formatting text.
+
     --plot_2D_embeddings  EXPERIMENTAL AND UNFINISHED. default to `False`. Will
                           compute 2D embeddins then create a 2D plots at the
                           end.
+
     --plot_dir PLOT_PATH  Path location for the output plots. Default is
                           'Plots'.
+
     --dist_metric DIST_METRIC
                           when computing the distance matrix, whether to use
                           'cosine' or 'rbf' or 'euclidean' metrics. cosine and
                           rbf should be fine. Default to 'cosine'
+
     --whole_deck_computation
                           defaults to `False`. This can only be used with TFIDF
                           and would not make any sense for sentence-
@@ -425,6 +463,7 @@ class AnnA:
                           the deck to feed into the vectorizer. Results in more
                           accurate relative distances between cards. (more
                           information at https://github.com/klieret/AnkiPandas)
+
     --enable_fuzz         Disable fuzzing when computing optimal order ,
                           otherwise a small random vector is added to the
                           reference_score and distance_score of each card. Note
@@ -432,6 +471,7 @@ class AnnA:
                           `score_adjustment_factor` then multiplied by the mean
                           distance then divided by 10 to make sure that it does
                           not overwhelm the other factors. Defaults to `True`.
+
     --resort_by_dist RESORT_BY_DIST
                           Resorting the new filtered deck taking onlyinto
                           account the semantic distance and not the reference
@@ -444,22 +484,27 @@ class AnnA:
                           to 'filter_review_cards'. Can be either 'farther' or
                           'closer' or False. 'farther' meaning to spread the
                           cards as evenly as possible. Default to 'closer'.
+
     --resort_split        If 'resort_by_dist' is not False, set to True to
                           resort the boosted cards separately from the rest and
                           make them appear first in the filtered deck. Default
                           to `False`.
+
     --profile_name PROFILE_NAME
                           defaults to `None`. Profile named used by ankipandas
                           to find your collection. If None, ankipandas will use
                           the most probable collection. Only used if
                           'whole_deck_computation' is set to `True`
+
     --disable_threading DISABLE_THREADING
                           defaults to `False`. If True will disable most hackish
                           multithreading of the code. Needed if you have a
                           terrible GPU that is slowing you down. This does not
                           apply to the plotting
+
     --keep_console_open   defaults to `False`. Set to True to open a python
                           console after running.
+
     --sync_behavior SYNC_BEHAVIOR
                           If contains 'before', will trigger a sync when AnnA is
                           run. If contains 'after', will trigger a sync at the
